e80fc6c8a0e555f2f1bb40fa30ce0e6a8827e537
Alexander Gladysh <agladysh@gmail.com>
2011-04-05 12:23:23 +0400

    make.sh: dumb build script (does not work)

---
+++ make.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+set -e
+
+echo "----> Going pedantic all over the source"
+
+echo "--> c89..."
+gcc -O2 -fPIC -I/usr/include/lua5.1 -c src/*.c -Isrc/ -Wall --pedantic -Werror --std=c89
+
+echo "--> c99..."
+gcc -O2 -fPIC -I/usr/include/lua5.1 -c src/*.c -Isrc/ -Wall --pedantic -Werror --std=c99
+
+echo "--> c++98..."
+gcc -xc++ -O2 -fPIC -I/usr/include/lua5.1 -c src/*.c -Isrc/ -Wall --pedantic -Werror --std=c++98
+
+echo "----> Making rock"
+sudo luarocks make rockspec/lua-geoip-scm-1.rockspec
