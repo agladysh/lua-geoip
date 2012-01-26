0813892d58fe550428020cb31bceb03abc1ebd98
Alexander Gladysh <agladysh@gmail.com>
2012-01-26 15:33:39 +0400

    make.sh: c{8,9}9 builds need -fms-extensions flag to compile
    As per TROUBLESHOOTING section of the GeoIP library README

--- make.sh
+++ make.sh
@@ -5,10 +5,10 @@ set -e
 echo "----> Going pedantic all over the source"
 
 echo "--> c89..."
-gcc -O2 -fPIC -I/usr/include/lua5.1 -c src/*.c -Isrc/ -Wall --pedantic -Werror --std=c89
+gcc -O2 -fPIC -I/usr/include/lua5.1 -c src/*.c -Isrc/ -Wall --pedantic -Werror --std=c89 -fms-extensions
 
 echo "--> c99..."
-gcc -O2 -fPIC -I/usr/include/lua5.1 -c src/*.c -Isrc/ -Wall --pedantic -Werror --std=c99
+gcc -O2 -fPIC -I/usr/include/lua5.1 -c src/*.c -Isrc/ -Wall --pedantic -Werror --std=c99 -fms-extensions
 
 echo "--> c++98..."
 gcc -xc++ -O2 -fPIC -I/usr/include/lua5.1 -c src/*.c -Isrc/ -Wall --pedantic -Werror --std=c++98
